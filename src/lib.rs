2debb3d33abc2ba3c8fe3d384e1bc2c824386625
Kevin Läufer <laeufer@cornell.edu>
2024-10-18 13:46:13 -0400

    add simple container

--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,12 @@ pub type WidthInt = u32;
 /// Word size for values.
 pub type Word = u64;
 
+/// Wraps either an array or a bit vector value.
+#[derive(Debug, Clone, PartialEq)]
+pub enum Value {
+    Array(ArrayValue),
+    BitVec(BitVecValue),
+}
+
 pub use array::*;
 pub use bv::*;
